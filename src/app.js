bbf180d6befe5d3b1edecb59cacfd8b35add5f26
Franco <francovacirca04@gmail.com>
2025-03-22 14:48:08 -0300

    Allow CORS from any source

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
 import cardRoutes from "./routes/cardRoutes.js";
+import cors from "cors";
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use("/api", cardRoutes);
 
--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,13 @@
 const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3333,
   archidektBaseUrl: "https://archidekt.com/api",
   mtgpiruloBaseUrl: "https://www.mtgpirulo.com",
   dealersBaseUrl: "https://www.magicdealersstore.com",
   tolariaBaseUrl: "https://www.tolariashop.com",
+  corsOptions: {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  },
 };
 
 export default config;
